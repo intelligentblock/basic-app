e9a1f13743942e44489b4b16ff9cf00653587cf1
intelligentblock <intelligentblock2018@gmail.com>
2019-07-16 22:05:02 -0400

    oomit Meesage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Users from './Users';
+import Contact from './Contact';
+import Math from './Math';
 import * as serviceWorker from './serviceWorker';
+import SimpleTabs from './SimpleTabs';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <SimpleTabs />
+            <Route exact path="/" component={App} />
+            <Route exact path="/Math" component={Math} />
+            <Route path="/Users" component={Users} />
+            <Route path="/Contact" component={Contact} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
